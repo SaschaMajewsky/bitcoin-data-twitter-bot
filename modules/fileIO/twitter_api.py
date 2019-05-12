867e422b31d71967537a051190094c560eaef8da
Sascha <sascha.majewsky93@gmx.de>
2019-05-12 19:01:14 +0200

    added gitignore, separation of folders and fundamental structure to compose a tweet

---
+++ modules/fileIO/twitter_api.py
@@ -0,0 +1,13 @@
+
+
+def get_consumer_key():
+    return 'test'
+
+def get_consumer_secret():
+    return 'test'
+
+def get_access_key():
+    return 'test'
+
+def get_access_secret():
+    return 'test'
---
+++ modules/twitter/tweet.py
@@ -0,0 +1,16 @@
+import tweepy
+import modules.fileIO.twitter_api
+
+
+CONSUMER_KEY = modules.fileIO.twitter_api.get_consumer_key()
+CONSUMER_SECRET = modules.fileIO.twitter_api.get_consumer_secret()
+ACCESS_KEY = modules.fileIO.twitter_api.get_access_key()
+ACCESS_SECRET = modules.fileIO.twitter_api.get_access_secret()
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+print(api)
+
+api.update_status('Test')
