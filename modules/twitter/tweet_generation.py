01cf94d227e00d1b246371f62dacec4ffad1f048
Sascha <sascha.majewsky93@gmx.de>
2019-05-13 14:14:17 +0200

    refactored the text generation of a tweets

--- modules/twitter/tweet_generation.py
+++ modules/twitter/tweet_generation.py
@@ -1,11 +1,20 @@
 from modules.twitter.tweet import send_tweet
 
-def generate_tweet_bitcoin_halving_progression(subsidy_era, block_subsidy, remaining_blocks, remaining_days, percentage_progression):
-    return "𝗕𝗜𝗧𝗖𝗢𝗜𝗡 𝗛𝗔𝗟𝗩𝗘𝗡𝗜𝗡𝗚 𝗣𝗥𝗢𝗚𝗥𝗘𝗦𝗦𝗜𝗢𝗡:\n" + "Subsidy Era: " + str(subsidy_era) + "/34\nBlock Subsidy Reward: " +\
-           str(block_subsidy) + " ₿\n" + "Remaining Blocks: " + str(remaining_blocks) + " ⬛" + " \nRemaining Days: " +\
-           "~" + str(remaining_days) + " ⌛\n" + "Progression until next Era: " + str(percentage_progression)\
-           + " %" + "\n\n" + "More Info:\n" + "https://bit.ly/1pQPBGe"
+
+def generate_tweet_bitcoin_halving_progression(subsidy_era, block_subsidy, remaining_blocks, remaining_days,
+                                               percentage_progression):
+    return "𝗕𝗧𝗖 𝗛𝗔𝗟𝗩𝗜𝗡𝗚 𝗜𝗡𝗙𝗢:\n" + "Subsidy Era: " + str(subsidy_era) + "/34\nBlock Subsidy Reward: " +\
+           str(block_subsidy) + " ₿\n" + "Progression until next Era: " + str(percentage_progression)\
+           + " %" + "\nRemaining: Blocks " + str(remaining_blocks) + " ⬜" + " Days " +\
+           "~" + str(remaining_days) + " ⌛\n" + generate_ascii_progress_bar(percentage_progression) +\
+           "\nInfo:\n" + "https://bit.ly/1pQPBGe"
+
+
+def generate_ascii_progress_bar(percentage_given):
+    filled_bar_length = int(round(26 * percentage_given / float(100)))
+    return '▓' * filled_bar_length + '░' * (26 - filled_bar_length)
+
 
 print(generate_tweet_bitcoin_halving_progression(3, 12.5, 54246, 377, 72.95))
 
-#send_tweet(generate_tweet_bitcoin_halving_progression(3, 12.5, 54246, 377, 72.95))
+send_tweet(generate_tweet_bitcoin_halving_progression(3, 12.5, 54246, 377, 72.95))
